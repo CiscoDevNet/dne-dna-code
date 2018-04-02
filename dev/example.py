2fae5382b197e465e31af8446589dea5ab6f35d1
Chris Lunsford <chrlunsf@cisco.com>
2018-04-02 17:13:27 -0400

    Update the Copyright Notice
    The copyright should be attributed to "Cisco and/or its affiliates."

--- dev/example.py
+++ dev/example.py
@@ -10,7 +10,8 @@ Script Dependencies:
 Depencency Installation:
     $ pip install requests
 
-Copyright (c) 2018, Cisco Systems, Inc. All rights reserved.
+
+Copyright (c) 2018 Cisco and/or its affiliates.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
