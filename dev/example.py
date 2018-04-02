720c352cb8cce24ff07d9d3d79c92471d4ee9a7a
Chris Lunsford <chrlunsf@cisco.com>
2018-04-02 16:45:44 -0400

    Initial Python Environment Files and Script Template
    Initial `env_lab.py`lab environment file.
    
    Initial `env_user.template` template user environment file.
    
    Initial `example.py` developer script template.

---
+++ dev/example.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""One-line summary of your script.
+
+Multi-line description of your script (make sure you include the copyright and
+license notice).
+
+Script Dependencies:
+    requests
+
+Depencency Installation:
+    $ pip install requests
+
+Copyright (c) 2018, Cisco Systems, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import os
+import sys
+
+import ciscosparkapi
+
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the absolute path for the project / repository root
+project_root = os.path.abspath(os.path.join(here, "../.."))
+
+
+# Extend the system path to include the project root and import the env files
+sys.path.insert(0, project_root)
+import env_lab      # noqa
+import env_user     # noqa
+
+
+spark = ciscosparkapi.CiscoSparkAPI(access_token=env_user.SPARK_ACCESS_TOKEN)
+
+
+print("""
+My Lab Environment:
+    DNA Center Host: {dnac_host}
+    DNA Center Username: {dnac_user}
+    DNA Center Password: {dnac_pass}
+""".format(
+    dnac_host=env_lab.DNA_CENTER["host"],
+    dnac_user=env_lab.DNA_CENTER["username"],
+    dnac_pass=env_lab.DNA_CENTER["password"],
+))
+
+
+print("Oh yeah... I'm also connected to Cisco Spark as:")
+print(spark.people.me())
+
+print("...and I'm posting things to the following Spark Room:")
+print(spark.rooms.get(env_user.SPARK_ROOM_ID))
---
+++ env_lab.py
@@ -0,0 +1,61 @@
+"""Set the Environment Information Needed to Access Your Lab!
+
+The provided sample code in this repository will reference this file to get the
+information needed to connect to your lab backend.  You provide this info here
+once and the scripts in this repository will access it as needed by the lab.
+
+
+Copyright (c) 2018, Cisco Systems, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+# User Input
+
+# Please select the lab environment that you will be using today
+#     sandbox - Cisco DevNet Always-On / Reserved Sandboxes
+#     express - Cisco DevNet Express Lab Backend
+#     custom  - Your Own "Custom" Lab Backend
+ENVIRONMENT_IN_USE = "sandbox"
+
+# Custom Lab Backend
+DNA_CENTER = {
+    "host": "",
+    "username": "",
+    "password": "",
+}
+
+# End User Input
+
+
+# Set the 'Environment Variables' based on the lab environment in use
+if ENVIRONMENT_IN_USE == "sandbox":
+    DNA_CENTER = {
+        "host": "",
+        "username": "",
+        "password": "",
+    }
+
+elif ENVIRONMENT_IN_USE == "express":
+    DNA_CENTER = {
+        "host": "",
+        "username": "",
+        "password": "",
+    }
