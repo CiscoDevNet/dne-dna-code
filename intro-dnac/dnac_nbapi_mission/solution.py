856f6c060ee19b155304d54ec5d7d27cb603e636
swiss-bruno <bklauser@gmx.ch>
2018-04-03 16:23:42 +0200

    Sample solution DNAC devices and modules - input

--- intro-dnac/dnac_nbapi_mission/solution.py
+++ intro-dnac/dnac_nbapi_mission/solution.py
@@ -64,6 +64,10 @@ dnac_user=env_lab.DNA_CENTER["username"]
 dnac_pass=env_lab.DNA_CENTER["password"]
 dnac_headers={'content-type': 'application/json'}
 
+next_data_file='next-data.js'
+next_data_file_header='/*DO NOT EDIT - NeXt Topology file generated from DNA Center Device and Module Inventory*/\n'
+next_data_file_footer='/*DO NOT EDIT - EOF*/\n'
+
 # create spark session object
 spark = ciscosparkapi.CiscoSparkAPI(access_token=env_user.SPARK_ACCESS_TOKEN)
 print("Oh yeah... I'm also connected to Cisco Spark as:")
@@ -74,7 +78,7 @@ print(spark.rooms.get(env_user.SPARK_ROOM_ID))
 def dnac_open_session(dnac_session, dnac_host, dnac_headers, dnac_username, dnac_password):
 
     # DNAC Login request to obtain session cookie
-    print('DNAC Login: login to ' + dnac_host + ' as ' + dnac_username + ' ...')
+    print('DNAC Login to ' + dnac_host + ' as ' + dnac_username + ' ...')
     dnac_auth_api='https://%s/api/system/v1/auth/login' % dnac_host
     r = dnac_session.get(dnac_auth_api, verify=False, headers=dnac_headers, auth=HTTPBasicAuth(dnac_username, dnac_password))
     r.raise_for_status()
@@ -83,6 +87,8 @@ def dnac_open_session(dnac_session, dnac_host, dnac_headers, dnac_username, dnac
 
     # extract cookie from login response headers and add to session object
     session_token_val = ((r.headers['Set-Cookie']).split('=')[1]).split(';')[0]
+    # session_token_val = r.json()["Token"]
+
     cookies = { "X-JWT-ACCESS-TOKEN" : session_token_val }
     dnac_session.cookies.update(cookies)
     print('DNAC Login: Session Cookie : ' + str(cookies))
@@ -97,6 +103,14 @@ def dnac_get_device_count(dnac_session, dnac_host, dnac_headers):
     # print('DNAC Response Body: ' + r.text)
     return r.json()['response']
 
+def dnac_get_devices(dnac_session, dnac_host, dnac_headers):
+    """DNAC Network Devices"""
+    tmp_url = 'https://%s/api/v1/network-device' % dnac_host
+    r = dnac_session.get(tmp_url, verify=False, headers=dnac_headers)
+    r.raise_for_status()
+    # print('DNAC Response Body: ' + r.text)
+    return r.json()['response']
+
 def dnac_get_host_count(dnac_session, dnac_host, dnac_headers):
     """DNAC Host Count"""
     tmp_url = 'https://%s/api/v1/host/count' % dnac_host
@@ -105,14 +119,70 @@ def dnac_get_host_count(dnac_session, dnac_host, dnac_headers):
     # print('DNAC Response Body: ' + r.text)
     return r.json()['response']
 
+def dnac_get_module_count(dnac_session, dnac_host, dnac_headers, device_id):
+    """DNAC Module Count of a Network Device"""
+    tmp_url = 'https://%s/api/v1/network-device/module/count' % dnac_host
+    tmp_params = {'deviceId' : device_id}
+
+    r = dnac_session.get(tmp_url, verify=False, headers=dnac_headers, params=tmp_params)
+    r.raise_for_status()
+    # print('DNAC Response Body: ' + r.text)
+    return r.json()['response']
+
+def dnac_get_modules(dnac_session, dnac_host, dnac_headers, device_id):
+    """DNAC Modules of a Network Device"""
+    tmp_url = 'https://%s/api/v1/network-device/module' % dnac_host
+    tmp_params = {'deviceId' : device_id}
+
+    r = dnac_session.get(tmp_url, verify=False, headers=dnac_headers, params=tmp_params)
+    r.raise_for_status()
+    # print('DNAC Response Body: ' + r.text)
+    return r.json()['response']
 
 
 
 with requests.Session() as dnac_session:
     r = dnac_open_session(dnac_session, dnac_host, dnac_headers, dnac_user, dnac_pass)
 
-    # DNAC Device Count and Host Count verbose after login
+    # Getting DNAC Device Count and Host Count verbose after login
     c = dnac_get_device_count(dnac_session, dnac_host, dnac_headers)
     print('DNAC Network Device Count: ' + str(c))
     c = dnac_get_host_count(dnac_session, dnac_host, dnac_headers)
     print('DNAC Host Count: ' + str(c))
+
+    # Getting DNAC Network devices with modules
+    devices = dnac_get_devices(dnac_session, dnac_host, dnac_headers)
+    devices_with_modules={}
+    i = 0
+
+    for d in devices:
+        c = dnac_get_module_count(dnac_session, dnac_host, dnac_headers, d['id'])
+        if c > 1:
+            print('Device ' + d['id'] + ' has '+str(c)+' modules')
+            i += 1
+            devices_with_modules[i] = {}
+            devices_with_modules[i]['modules'] = {}
+            devices_with_modules[i]['modules']['count'] = c
+            devices_with_modules[i]['nextid'] = i
+            devices_with_modules[i]['dnacid'] = d['id']
+            devices_with_modules[i]['platformId'] = d['platformId']
+            devices_with_modules[i]['serialNumber'] = d['serialNumber']
+            devices_with_modules[i]['hostname'] = d['hostname']
+
+            di = i
+            modules=dnac_get_modules(dnac_session, dnac_host, dnac_headers, d['id'])
+            for m in modules:
+                i += 1
+                devices_with_modules[di]['modules'][i] = {}
+                devices_with_modules[di]['modules'][i]['nextid'] = i
+                devices_with_modules[di]['modules'][i]['dnacid'] = m['id']
+                devices_with_modules[di]['modules'][i]['partNumber'] = m['partNumber']
+                devices_with_modules[di]['modules'][i]['serialNumber'] = m['serialNumber']
+
+    pprint(devices_with_modules)
+
+    with open(next_data_file, 'w', encoding="utf-8") as outfile:
+        print('Writing NeXt UI Topology Data File')
+        outfile.write(next_data_file_header)
+
+        outfile.write(next_data_file_footer)
