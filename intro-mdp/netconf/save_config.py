b1bd4a74fa772ed95c16394c1d57fed633e66396
hpreston <hank.preston@gmail.com>
2018-04-04 16:55:08 -0400

    NETCONF Lab code samples

---
+++ intro-mdp/netconf/save_config.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""This script saves the running configuration on an IOS XE device.
+
+Copyright (c) 2018 Cisco and/or its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import os
+import sys
+from ncclient import manager, xml_
+import xmltodict
+import xml.dom.minidom
+
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the absolute path for the project / repository root
+project_root = os.path.abspath(os.path.join(here, "../.."))
+
+
+# Extend the system path to include the project root and import the env files
+sys.path.insert(0, project_root)
+import env_lab  # noqa
+
+# Create an XML body to execute the save operation
+save_body = """
+<cisco-ia:save-config xmlns:cisco-ia="http://cisco.com/yang/cisco-ia"/>
+"""
+
+print("Opening NETCONF Connection to {}".format(env_lab.IOS_XE_1["host"]))
+
+# Open a connection to the network device using ncclient
+with manager.connect(
+        host=env_lab.IOS_XE_1["host"],
+        port=env_lab.IOS_XE_1["netconf_port"],
+        username=env_lab.IOS_XE_1["username"],
+        password=env_lab.IOS_XE_1["password"],
+        hostkey_verify=False
+        ) as m:
+
+    print("Sending a RPC operation to the device.\n")
+    # Use ncclient to send the RPC operation
+    netconf_reply = m.dispatch(xml_.to_ele(save_body))
+
+print("Here is the raw XML data returned from the device.\n")
+# Print out the raw XML that returned
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+print("")
