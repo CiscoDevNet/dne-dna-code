6e69298bb98026b15322b66935e9d87db6d98a84
Chris Lunsford <chrlunsf@cisco.com>
2018-04-05 03:34:05 -0400

    Solutions for intro-python/part1/hands_on_exercise.py

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,24 +7,39 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print("pi is a {} with a value of {}".format(type(pi), pi))
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+if i < 50:
+    print("i is less than 50")
+elif i == 50:
+    print("i is equal to 50")
+else:
+    print("i is greater than 50")
 
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print("The fruit is orange")
+elif picked_fruit == 'strawberry':
+    print("The fruit is red")
+elif picked_fruit == 'banana':
+    print("The fruit is yellow")
 
 
 # TODO: Write a function that multiplies two numbers and returns the result
-# Define the function here.
+def multiply(num1, num2):
+    """Multiply two numbers and return the result."""
+    result = num1 * num2
+    return result
 
 
 # TODO: Now call the function a few times to calculate the following answers
+print("12 x 96 =", multiply(12, 96))
 
-print("12 x 96 =",)
+print("48 x 17 =", multiply(48, 17))
 
-print("48 x 17 =",)
-
-print("196523 x 87323 =",)
+print("196523 x 87323 =", multiply(196523, 87323))
