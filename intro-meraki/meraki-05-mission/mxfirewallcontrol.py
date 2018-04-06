1146ef989b66b42a1964fc2c36505e7c15c81ea5
mdenapol <denapom11@gmail.com>
2018-04-06 14:20:42 -0400

    mission solution

--- intro-meraki/meraki-05-mission/mxfirewallcontrol.py
+++ intro-meraki/meraki-05-mission/mxfirewallcontrol.py
@@ -1,16 +1,22 @@
 """Set the Environment Information Needed to Access Your Lab!
+
 The provided sample code in this repository will reference this file to get the
 information needed to connect to your lab backend.  You provide this info here
 once and the scripts in this repository will access it as needed by the lab.
+
+
 Copyright (c) 2018 Cisco and/or its affiliates.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -264,7 +270,7 @@ def readmxfwruleset(p_apikey, p_nwid):
     time.sleep(API_EXEC_DELAY)
     try:
         r = requests.get(
-            "https://api.meraki.com/api/v0/networks/%s/firewallrules"
+            "https://api.meraki.com/api/v0/networks/%s/l3FirewallRules"
             % (p_nwid),
             headers={
                 "X-Cisco-Meraki-API-Key": p_apikey,
