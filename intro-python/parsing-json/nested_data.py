247bc1749ac164c43685b1ab5af40ce8894476e3
Chris Lunsford <chrlunsf@cisco.com>
2018-04-10 18:18:46 -0400

    Solution for intro-python/parsing-json/nested_data.py Hands-On Exercise

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -12,7 +12,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "interfaces.json")) as file:
     # TODO: Parse the contents of the JSON file into a variable
-
+    json_data = json.loads(file.read())
 
 # TODO: Loop through the interfaces in the JSON data and print out each
 # interface's name, ip, and netmask.
+for interface in json_data["ietf-interfaces:interfaces"]["interface"]:
+    print("{name}: {ip} {netmask}".format(
+        name=interface["name"],
+        ip=interface["ietf-ip:ipv4"]["address"][0]["ip"],
+        netmask=interface["ietf-ip:ipv4"]["address"][0]["netmask"],
+    ))
