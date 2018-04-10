88e0af72044afbf23e4a64d17e3e062db0426cd2
Chris Lunsford <chrlunsf@cisco.com>
2018-04-10 18:14:20 -0400

    Initial intro-python/parsing-json Sample Code

---
+++ intro-python/parsing-json/nested_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Working with nested data hands-on exercise / coding challenge."""
+
+
+import json
+import os
+
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(here, "interfaces.json")) as file:
+    # TODO: Parse the contents of the JSON file into a variable
+
+
+# TODO: Loop through the interfaces in the JSON data and print out each
+# interface's name, ip, and netmask.
