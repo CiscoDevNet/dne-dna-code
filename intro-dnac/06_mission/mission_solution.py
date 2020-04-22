8c97c1ef9dbc7d283d61fc095d0f93a2f259f689
Kareem Iskander (kiskande) <kiskande@cisco.com>
2020-04-22 14:27:29 -0700

    updating to point to latest DNAC Sandbox and fixed a bug

--- intro-dnac/06_mission/mission_solution.py
+++ intro-dnac/06_mission/mission_solution.py
@@ -1,9 +1,20 @@
 import requests
 import os
 import json
+import sys
 from requests.auth import HTTPBasicAuth  # for Basic Auth
-import env_lab
 import time
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the absolute path for the project / repository root
+project_root = os.path.abspath(os.path.join(here, "../.."))
+
+# Extend the system path to include the project root and import the env files
+sys.path.insert(0, project_root)
+
+import env_lab  # noqa
 
 DNAC_URL = env_lab.DNA_CENTER["host"]
 DNAC_USER = env_lab.DNA_CENTER["username"]
